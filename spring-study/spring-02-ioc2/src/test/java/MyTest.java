426f899db801aac2b0988d63dfda1c4f19465941
YunboLyu <vnborx@outlook.com>
2020-09-18 02:30:13 +0800

    🔨 Add more configuration files

--- spring-study/spring-02-ioc2/src/test/java/MyTest.java
+++ spring-study/spring-02-ioc2/src/test/java/MyTest.java
@@ -1,16 +1,21 @@
 import com.vnborx.pojo.User;
+import com.vnborx.pojo.User2;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
 //        User user = (User)context.getBean("user");
 //        user.show();
 
-        User u1 = (User)context.getBean("user");
-        User u2 = (User)context.getBean("user");
-        System.out.println(u1 == u2); // true
+//        User u1 = (User)context.getBean("user");
+//        User u2 = (User)context.getBean("user");
+//        System.out.println(u1 == u2); // true
 
+        User2 user = (User2)context.getBean("userE");
+        user.show();
     }
 }
