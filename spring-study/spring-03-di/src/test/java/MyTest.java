ea3b3e44a827c2e66b884a79d5174ebeb4aa09f2
YunboLyu <vnborx@outlook.com>
2020-09-21 03:17:38 +0800

    🔨 Add a new class to test p-namespace & c-namespace

--- spring-study/spring-03-di/src/test/java/MyTest.java
+++ spring-study/spring-03-di/src/test/java/MyTest.java
@@ -1,11 +1,21 @@
 import com.vnborx.pojo.Student;
+import com.vnborx.pojo.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Student student = (Student) context.getBean("student");
-        System.out.println(student.toString());
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//        Student student = (Student) context.getBean("student");
+//        System.out.println(student.toString());
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userBeans.xml");
+        User user = context.getBean("user2", User.class);
+        System.out.println(user);
     }
       
 }
