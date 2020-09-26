8bbe080f25db0a8ea18e15ec9544f35c59e08686
YunboLyu <vnborx@outlook.com>
2020-09-27 00:59:54 +0800

    🔨 Add a demo of annotation

--- spring-study/spring-04-autowired/src/main/java/com/vnborx/pojo/Person.java
+++ spring-study/spring-04-autowired/src/main/java/com/vnborx/pojo/Person.java
@@ -1,32 +1,31 @@
 package com.vnborx.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.lang.Nullable;
+
 public class Person {
     private String name;
+    @Autowired
+    @Qualifier(value = "catTest")
     private Cat cat;
+    @Autowired
     private Dog dog;
+
+//    public Person (@Nullable String name) {
+//        this.name = name;
+//    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Cat getCat() {
         return cat;
     }
 
-    public void setCat(Cat cat) {
-        this.cat = cat;
-    }
-
     public Dog getDog() {
         return dog;
-    }
-
-    public void setDog(Dog dog) {
-        this.dog = dog;
     }
 
     @Override
