0ad5aa17597d4b8f77b55c0514518cb6b1acd1dc
YunboLyu <vnborx@outlook.com>
2020-10-02 03:21:45 +0800

    🔨 Extend view resolver

---
+++ spring-boot-study/spring-boot-03-web/src/main/java/com/vnborx/config/MyMvcConfig.java
@@ -0,0 +1,25 @@
+package com.vnborx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Locale;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver myViewResolver() {
+        return new MyViewResolver();
+    }
+
+    public static class MyViewResolver implements ViewResolver {
+        @Override
+        public View resolveViewName(String viewName, Locale locale) throws Exception {
+            return null;
+        }
+    }
+}
