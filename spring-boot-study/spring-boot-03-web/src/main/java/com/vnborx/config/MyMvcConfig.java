75512dc8ad3bb55766ddea610660d5a062ff904d
YunboLyu <vnborx@outlook.com>
2020-10-03 03:19:55 +0800

    🔨 Extend Spring MVC

--- spring-boot-study/spring-boot-03-web/src/main/java/com/vnborx/config/MyMvcConfig.java
+++ spring-boot-study/spring-boot-03-web/src/main/java/com/vnborx/config/MyMvcConfig.java
@@ -1,25 +1,15 @@
 package com.vnborx.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Locale;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ViewResolver myViewResolver() {
-        return new MyViewResolver();
-    }
-
-    public static class MyViewResolver implements ViewResolver {
-        @Override
-        public View resolveViewName(String viewName, Locale locale) throws Exception {
-            return null;
-        }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/vnborx").setViewName("test");
     }
 }
